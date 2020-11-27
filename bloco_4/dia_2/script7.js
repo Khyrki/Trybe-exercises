204cd58af25e03f2c5b4aaacc7aaae91c701b0ee
Khyrki <khyrkimitsunobi@gmail.com>
2020-11-27 15:34:16 -0300

    Adiciona pequenas alteracoes nos exercicios 11, 5 e 7

--- bloco_4/dia_2/script7.js
+++ bloco_4/dia_2/script7.js
@@ -1,5 +1,5 @@
 let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
-let menor = 99999999999999999999999999999999999999;
+let menor = numbers [0];
 
 for (let index = 0; index < numbers.length; index += 1) {
   if (numbers[index] < menor) {
