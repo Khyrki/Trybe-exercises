c2da7d7b0d8f1fe07c2088ccccc5224b64a0709c
Khyrki <khyrkimitsunobi@gmail.com>
2020-12-04 00:50:56 -0300

    adiciona o requisito 7 e 8

--- bloco_5_JavaScript-DOM-Eventos-WebStorage/dia_2_Trabalhando-com-Elementos/conteudo.js
+++ bloco_5_JavaScript-DOM-Eventos-WebStorage/dia_2_Trabalhando-com-Elementos/conteudo.js
@@ -12,5 +12,12 @@ requisito4.children[1].children[0];
 let requisito5 = document.getElementById('elementoOndeVoceEsta');
 requisito5.previousElementSibling;
 
+/*let requisito6 = document.getElementById('elementoOndeVoceEsta')
+console.log(requisito6.parentNode.innerText.includes('Atenção!'))*/
 
+let requisito7 = document.getElementById('elementoOndeVoceEsta');
+requisito7.nextElementSibling;
+
+let requisito8 = document.getElementById('pai');
+console.log(requisito8.children[2]);
 
