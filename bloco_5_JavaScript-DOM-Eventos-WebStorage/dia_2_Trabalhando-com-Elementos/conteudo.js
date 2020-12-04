85bc332e13f578567d741c6b60471039a5284e22
Khyrki <khyrkimitsunobi@gmail.com>
2020-12-03 23:58:01 -0300

    requisito 4

--- bloco_5_JavaScript-DOM-Eventos-WebStorage/dia_2_Trabalhando-com-Elementos/conteudo.js
+++ bloco_5_JavaScript-DOM-Eventos-WebStorage/dia_2_Trabalhando-com-Elementos/conteudo.js
@@ -1,7 +1,13 @@
-let paiStyle = document.getElementById('elementoOndeVoceEsta').parentNode;
+let requisito1 = document.getElementById('elementoOndeVoceEsta');
+
+let requisito2 = document.getElementById('elementoOndeVoceEsta').parentNode;
 paiStyle.style.backgroundColor = 'red';
 
-let primeiroFilho = document.getElementById('primeiroFilho');
+let requisito3 = document.getElementById('primeiroFilho');
 primeiroFilho.innerText = 'tudo bem com vc'
+
+let requisito4 = document.querySelector('#pai')
+console.log(paiprimeiroFilhoDoFilho.children[1].children[0]);
+
 
 
