a1ced70de6bde682e14eda3464536a04a780fab4
Khyrki <khyrkimitsunobi@gmail.com>
2020-12-04 22:05:37 -0300

    Adiciona exercicio 1 do dia 5.3

--- bloco_5_JavaScript-DOM-Eventos-WebStorage/dia_3_Eventos/script.js
+++ bloco_5_JavaScript-DOM-Eventos-WebStorage/dia_3_Eventos/script.js
@@ -14,12 +14,26 @@ function createDaysOfTheWeek() {
 createDaysOfTheWeek();
 // Escreva seu código abaixo.
 const dezDaysList = [29, 30, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31];
+let daysOfWeek = document.querySelector('#days');
 
 for (let index = 0; index < dezDaysList.length; index += 1) {
-  let daysOfWeek = document.querySelector('#days');
   let li = document.createElement('li');
   li.className = 'day';
   li.innerText = dezDaysList[index]
   daysOfWeek.appendChild(li);
-
 }
+
+//let dias = document.querySelector('#days');
+let dia = daysOfWeek.children;
+for (let index = 0; index < dia.length; index += 1) {
+ if (dia[index].innerText === '24' || dia[index].innerText === '25' || dia[index].innerText === '31') {
+  dia[index].classList.add ('holiday');
+ } 
+
+ if (dia[index].innerText === '4' || dia[index].innerText === '11' || dia[index].innerText === '18' || dia[index].innerText === '25') {
+  dia[index].classList.add ('friday');
+ }
+}
+
+
+
