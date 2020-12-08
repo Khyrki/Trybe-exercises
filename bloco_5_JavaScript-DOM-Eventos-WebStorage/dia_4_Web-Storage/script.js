d055296707f79e8ef3cf592a5e558bdb95411aa4
Khyrki <khyrkimitsunobi@gmail.com>
2020-12-08 00:51:34 -0300

    Adiciona local storege para background color e cor do textoo

--- bloco_5_JavaScript-DOM-Eventos-WebStorage/dia_4_Web-Storage/script.js
+++ bloco_5_JavaScript-DOM-Eventos-WebStorage/dia_4_Web-Storage/script.js
@@ -1,23 +1,29 @@
 function changePageBackgroundColor () {
+  let corpo = document.getElementsByTagName('body')
   let header = document.querySelector('#header');
   let changeColorButton = document.createElement('button');
+  corpo[0].style.backgroundColor = localStorage.getItem('backgroundColor');
+  document.body.style.color = localStorage.getItem('fontColor');
 
   header.appendChild(changeColorButton);
   changeColorButton.innerText = 'Dark Mode'
 
   changeColorButton.addEventListener('click',function() {
-    let corpo = document.getElementsByTagName('body')
     
-    if (corpo[0].style.backgroundColor == 'rgb(240, 240, 240)') {
+    localStorage.setItem ('backgroundColor', 'rgb(240, 240, 240)')
+    localStorage.setItem ('fontColor', 'black')
+    if (corpo[0].style.backgroundColor === (localStorage.getItem('backgroundColor'))) {
       corpo[0].style.backgroundColor = 'black';
       corpo[0].style.color = 'white'
+      localStorage.backgroundColor = 'black'
+      localStorage.fontColor = 'white'
     } else {
       corpo[0].style.backgroundColor = 'rgb(240, 240, 240)';
       corpo[0].style.color = 'black'
+      localStorage.backgroundColor = 'rgb(240, 240, 240)'
+      localStorage.fontColor = 'black'
     }
   })
 }
 
 changePageBackgroundColor();
-
-console.log (document.querySelector('body').style.backgroundColor)
