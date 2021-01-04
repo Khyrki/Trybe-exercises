20cf62017e1cb4979f09db79bc8f05f1e9c7cb7b
Khyrki <khyrkimitsunobi@gmail.com>
2021-01-04 20:46:12 -0300

    delimita a data maxima como dia de hoje

--- bloco_6_HTML-CSS:Forms-Flexbox-Responsivo/Dia_1_HTML-CSS-Forms/script.js
+++ bloco_6_HTML-CSS:Forms-Flexbox-Responsivo/Dia_1_HTML-CSS-Forms/script.js
@@ -9,3 +9,17 @@ function createStateList() {
   }
 }
 createStateList();
+
+let today = new Date();
+let dd = today.getDate();
+let mm = today.getMonth()+1; //January is 0!
+let yyyy = today.getFullYear();
+ if(dd<10){
+        dd='0'+dd
+    } 
+    if(mm<10){
+        mm='0'+mm
+    } 
+
+today = yyyy+'-'+mm+'-'+dd;
+document.getElementById("date").setAttribute("max", today);
