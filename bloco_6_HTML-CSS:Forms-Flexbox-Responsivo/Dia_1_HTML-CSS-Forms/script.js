bf40da832d9fbd2211e85698a6663412af395447
Khyrki <khyrkimitsunobi@gmail.com>
2021-01-05 11:09:15 -0300

    finaliza o formulario

--- bloco_6_HTML-CSS:Forms-Flexbox-Responsivo/Dia_1_HTML-CSS-Forms/script.js
+++ bloco_6_HTML-CSS:Forms-Flexbox-Responsivo/Dia_1_HTML-CSS-Forms/script.js
@@ -1,5 +1,9 @@
 const estados = document.querySelector('#estado');
 const listaEstados = ['Acre','Alagoas','Amapá','Amazonas','Bahia','Ceará','Espírito Santo','Goiás','Maranhão','Mato Grosso','Mato Grosso do Sul','Minas Gerais','Pará','Paraíba','Paraná','Pernambuco','Piauí','Rio de Janeiro','Rio Grande do Norte','Rio Grande do Sul','Rondônia','Roraima','Santa Catarina','São Paulo','Sergipe','Tocantins','Distrito Federal'];
+const sendButton = document.querySelector('#send');
+const recoverItems = document.querySelectorAll('.recover');
+const recoverDiv = document.querySelector('#recover-div');
+const cleanButton = document.querySelector('#clean');
 
 function createStateList() {
   for (let index = 0; index < listaEstados.length; index += 1) {
@@ -10,16 +14,50 @@ function createStateList() {
 }
 createStateList();
 
-let today = new Date();
-let dd = today.getDate();
-let mm = today.getMonth()+1; //January is 0!
-let yyyy = today.getFullYear();
- if(dd<10){
-        dd='0'+dd
-    } 
-    if(mm<10){
-        mm='0'+mm
-    } 
+function setMaxDate() {
+  let today = new Date();
+  let dd = today.getDate();
+  let mm = today.getMonth()+1; //January is 0!
+  let yyyy = today.getFullYear();
+   if(dd<10){
+          dd='0'+dd
+      } 
+      if(mm<10){
+          mm='0'+mm
+      } 
+  
+  today = yyyy+'-'+mm+'-'+dd;
+  document.getElementById("date").setAttribute("max", today);  
+}
+setMaxDate();
 
-today = yyyy+'-'+mm+'-'+dd;
-document.getElementById("date").setAttribute("max", today);
+function retrievesValues() {
+  sendButton.addEventListener('click', function (event) {
+    event.preventDefault();
+    for(let index = 0; index < recoverItems.length; index += 1) {
+      if (recoverItems[index].type === 'radio' && !recoverItems[index].checked) {
+        continue;
+      }
+      if(recoverItems[index].name === 'Estado:' || recoverItems[index].name === 'Resumo do currículo:') {
+        const newParagraph = document.createElement('p');
+      recoverDiv.appendChild(newParagraph);
+      newParagraph.innerText = `${recoverItems[index].name} ${recoverItems[index].value}`;
+      } else {
+        const newParagraph = document.createElement('p');
+        recoverDiv.appendChild(newParagraph);
+        newParagraph.innerText = `${recoverItems[index].alt} ${recoverItems[index].value}`;
+      }
+    }
+  });
+}
+retrievesValues();
+
+function cleanForm() {
+  cleanButton.addEventListener('click', function () {
+    for(let index = 0; index < recoverItems.length; index += 1) {
+      recoverItems[index].value = '';
+    }
+    recoverDiv.innerHTML = '';
+  });
+}
+cleanForm();
