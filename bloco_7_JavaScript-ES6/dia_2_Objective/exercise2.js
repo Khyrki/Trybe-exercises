436c12000349ef78d043b4e4eb9d1ade08ec6b2d
Khyrki <khyrkimitsunobi@gmail.com>
2021-01-13 19:10:07 -0300

    Resolve exercicio 1 parte II do bloco 7 dia 2

---
+++ bloco_7_JavaScript-ES6/dia_2_Objective/exercise2.js
@@ -0,0 +1,26 @@
+const lesson1 = {
+  materia: 'Matemática',
+  numeroEstudantes: 20,
+  professor: 'Maria Clara',
+  turno: 'manhã',
+};
+
+const lesson2 = {
+  materia: 'História',
+  numeroEstudantes: 20,
+  professor: 'Carlos',
+};
+
+const lesson3 = {
+  materia: 'Matemática',
+  numeroEstudantes: 10,
+  professor: 'Maria Clara',
+  turno: 'noite',
+};
+
+const turnoManha = (objeto, chave, valor) => {
+  objeto[chave] = valor;
+  return objeto;
+}
+
+console.log(turnoManha(lesson2, 'turno', 'manhã'));
