d76e54cb603354ba933046925d88e1a016dc1bf6
Khyrki <khyrkimitsunobi@gmail.com>
2021-01-14 22:04:06 -0300

    Adiciona exercicio 7 bloco 7 dia 3

---
+++ bloco_7_JavaScript-ES6/dia_3_Testes-unitarios/exercicio7.js
@@ -0,0 +1,20 @@
+const assert = require('assert');
+// escreva a função wordLengths aqui
+const wordLengths = (words) => {
+  const output = [];
+  for (let index = 0; index < words.length; index += 1) {
+    output.push(words[index].length);
+  }
+
+  return output;
+}
+
+
+
+const words = ['sun', 'potato', 'roundabout', 'pizza'];
+const expected = [3, 6, 10, 5];
+
+assert.strictEqual(typeof wordLengths, 'function');
+const output = wordLengths(words);
+assert.deepStrictEqual(output, expected);
+
