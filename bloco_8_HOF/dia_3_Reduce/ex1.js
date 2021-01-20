12b02db3fe1dc117cf281c6e1d05aed8272f09b8
Khyrki <khyrkimitsunobi@gmail.com>
2021-01-20 18:49:39 -0300

    Adicionad Exercício 1 do Bloco 8 dia 3

---
+++ bloco_8_HOF/dia_3_Reduce/ex1.js
@@ -0,0 +1,20 @@
+
+const assert = require('assert');
+
+const arrays = [
+    ["1", "2", "3"],
+    [true],
+    [4, 5, 6]
+];
+
+
+function flatten() {
+  // escreva seu código aqui
+  
+  const newMatriz = arrays.reduce((accumulator, currentValue) => {
+    return accumulator.concat(currentValue);
+  }, []);
+  return newMatriz
+}
+
+assert.deepStrictEqual(flatten(), ["1", "2", "3", true, 4, 5, 6]);
