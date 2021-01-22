4fecf846b809c98887a190d278ff89492e793cd0
Khyrki <khyrkimitsunobi@gmail.com>
2021-01-21 21:50:51 -0300

    Adicionad Exercício 4 do Bloco 8 dia 4

---
+++ bloco_8_HOF/dia_4_Spread-operator-parâmetro-Rest-Destructuring-Defoult/ex4.js
@@ -0,0 +1,38 @@
+const assert = require('assert')
+
+const people = [
+    {
+        name: "Nicole",
+        bornIn: 1992,
+        nationality: "Australian"
+    },
+    {
+        name: "Harry",
+        bornIn: 2008,
+        nationality: "Australian"
+    },
+    {
+        name: "Toby",
+        bornIn: 1901,
+        nationality: "Australian"
+    },
+    {
+        name: "Frida",
+        bornIn: 1960,
+        nationality: "Dannish"
+    },
+    {
+        name: "Fernando",
+        bornIn: 2001,
+        nationality: "Brazilian"
+    }
+]
+
+// escreva filterPeople abaixo
+
+const filteredPeople = people.filter((people) => {
+    return people.nationality === 'Australian' && people.bornIn > 1900 && people.bornIn < 2001
+  })
+
+assert.deepStrictEqual(filteredPeople[0], { name: "Nicole", bornIn: 1992, nationality: "Australian" })
+assert.deepStrictEqual(filteredPeople[1], { name: "Toby", bornIn: 1901, nationality: "Australian" })
