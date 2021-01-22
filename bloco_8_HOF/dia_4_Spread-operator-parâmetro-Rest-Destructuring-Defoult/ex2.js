59fd343b2c3fd200747aa8e767914d0b42597ba3
Khyrki <khyrkimitsunobi@gmail.com>
2021-01-21 21:20:16 -0300

    Adicionad Exercício 2 do Bloco 8 dia 4

---
+++ bloco_8_HOF/dia_4_Spread-operator-parâmetro-Rest-Destructuring-Defoult/ex2.js
@@ -0,0 +1,14 @@
+const assert = require('assert')
+
+// escreva sum abaixo
+const sum = (...rest) => {
+  return rest.reduce((accumulator, currentValue) => {
+    return accumulator += currentValue;
+  }, 0)
+}
+
+assert.strictEqual(sum(), 0)
+assert.strictEqual(sum(1), 1)
+assert.strictEqual(sum(1, 2), 3)
+assert.strictEqual(sum(1, 2, 3), 6)
+assert.strictEqual(sum(1, 2, 3, 4), 10)
