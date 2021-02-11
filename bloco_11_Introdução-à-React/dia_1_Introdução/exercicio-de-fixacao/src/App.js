e05e9e1491c1883589844a1c3db9fe8b0aa429df
Khyrki <khyrkimitsunobi@gmail.com>
2021-02-11 20:22:08 -0300

    Exercício de fixacao react do gatinho

--- bloco_11_Introdução-à-React/dia_1_Introdução/exercicio-de-fixacao/src/App.js
+++ bloco_11_Introdução-à-React/dia_1_Introdução/exercicio-de-fixacao/src/App.js
@@ -1,8 +1,9 @@
 import React from 'react';
-import AboutMe from './Component'
+//import AboutMe from './Component'
+import Image from './Image'
 
 function App() {
-  return <AboutMe />
+  return <Image source ='https://cdn.pixabay.com/photo/2017/02/20/18/03/cat-2083492_1280.jpg' alternativeText = 'Cute cat staring'/>
 }
 
 export default App;
