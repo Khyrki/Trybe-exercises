25e39a167a6eabcbc551dde9ddd64d9d4c48536f
Khyrki <khyrkimitsunobi@gmail.com>
2021-02-19 13:01:10 -0300

    finaliza pokedex (sem stilo)

--- bloco_11_Introdução-à-React/dia_2_Componestes-React/my-pokedex/src/App.js
+++ bloco_11_Introdução-à-React/dia_2_Componestes-React/my-pokedex/src/App.js
@@ -1,23 +1,13 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import pokemons from './data'
+import Pokedex from './components/pokedex'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Pokedex</h1>
+      <Pokedex pokemons ={ pokemons }/>
     </div>
   );
 }
