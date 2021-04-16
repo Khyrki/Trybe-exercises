34ca1c5788e7c21e59b4d2a6a89b123c1a09e4ba
Khyrki <khyrkimitsunobi@gmail.com>
2021-04-16 20:18:05 -0300

    refatora exercico do dia 18.1 removendo redux e implementando Context API

--- bloco_18_Context-API-e-React-Hooks/dia_1_Context-API-do-React/exercise-contextAPI-refactoring-exercise-one/src/App.js
+++ bloco_18_Context-API-e-React-Hooks/dia_1_Context-API-do-React/exercise-contextAPI-refactoring-exercise-one/src/App.js
@@ -1,11 +1,38 @@
 import React from 'react';
 import './App.css';
 import Cars from './Cars';
+import MyContext from './Context/MyContext';
 
-function App() {
-  return (
-    <Cars />
-  );
+class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      redCar: false,
+      blueCar: false,
+      yellowCar: false,
+    }
+    this.moveCar = this.moveCar.bind(this);
+  }
+  
+  moveCar(carColor, carAction) {
+    this.setState({
+      [carColor]: carAction,
+    })
+  }
+
+  render() {
+    const contextValue = {
+      redCar: this.state.redCar,
+      blueCar: this.state.blueCar,
+      yellowCar: this.state.yellowCar,
+      moveCar: this.moveCar,
+    }
+    return (
+      <MyContext.Provider value={ contextValue }>
+        <Cars />
+      </MyContext.Provider>
+    );
+  }
 }
 
 export default App;
--- bloco_18_Context-API-e-React-Hooks/dia_1_Context-API-do-React/exercise-contextAPI-refactoring-exercise-one/src/Cars.jsx
+++ bloco_18_Context-API-e-React-Hooks/dia_1_Context-API-do-React/exercise-contextAPI-refactoring-exercise-one/src/Cars.jsx
@@ -1,71 +1,62 @@
 // src/Cars.jsx
-
 import PropTypes from 'prop-types';
 import React from 'react';
-import { connect } from 'react-redux';
 import carBlue from './images/carBlue.jpeg';
 import carRed from './images/carRed.jpeg';
 import carYellow from './images/carYellow.jpeg';
-import { moveCar } from './redux/actionCreators';
+import MyContext from './Context/MyContext';
 
-function Cars({ redCar, blueCar, yellowCar, moveCar }) {
-  return (
-    <div>
+class Cars extends React.Component {
+  render() {
+    const { redCar, blueCar, yellowCar, moveCar } = this.context;
+    console.log()
+    return (
       <div>
-        <img
-          className={redCar ? 'car-right' : 'car-left'}
-          src={carRed}
-          alt="red car"
-        />
-        <button
-          onClick={() => moveCar('red', !redCar)}
-          type="button"
-        >
-          Move
-        </button>
+        <div>
+          <img
+            className={redCar ? 'car-right' : 'car-left'}
+            src={carRed}
+            alt="red car"
+          />
+          <button
+            onClick={() => moveCar('redCar', !redCar)}
+            type="button"
+          >
+            Move
+          </button>
+        </div>
+        <div>
+          <img
+            className={blueCar ? 'car-right' : 'car-left'}
+            src={carBlue}
+            alt="blue car"
+          />
+          <button
+            onClick={() => moveCar('blueCar', !blueCar)}
+            type="button"
+          >
+            Move
+          </button>
+        </div>
+        <div>
+          <img
+            className={yellowCar ? 'car-right' : 'car-left'}
+            src={carYellow}
+            alt="yellow car"
+          />
+          <button
+            onClick={() => moveCar('yellowCar', !yellowCar)}
+            type="button"
+          >
+            Move
+          </button>
+        </div>
       </div>
-      <div>
-        <img
-          className={blueCar ? 'car-right' : 'car-left'}
-          src={carBlue}
-          alt="blue car"
-        />
-        <button
-          onClick={() => moveCar('blue', !blueCar)}
-          type="button"
-        >
-          Move
-        </button>
-      </div>
-      <div>
-        <img
-          className={yellowCar ? 'car-right' : 'car-left'}
-          src={carYellow}
-          alt="yellow car"
-        />
-        <button
-          onClick={() => moveCar('yellow', !yellowCar)}
-          type="button"
-        >
-          Move
-        </button>
-      </div>
-    </div>
-  );
+    );
+  }
 }
 
-Cars.propTypes = {
-  moveCar: PropTypes.func.isRequired,
-  blueCar: PropTypes.bool.isRequired,
-  redCar: PropTypes.bool.isRequired,
-  yellowCar: PropTypes.bool.isRequired,
-};
+Cars.contextType = MyContext
 
-const mapStateToProps = (state) => ({
-  redCar: state.cars.red,
-  blueCar: state.cars.blue,
-  yellowCar: state.cars.yellow});
 
-const mapDispatchToProps = { moveCar };
-
-export default connect(mapStateToProps, mapDispatchToProps)(Cars);
+export default Cars;
