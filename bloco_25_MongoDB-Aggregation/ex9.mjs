ab8aa391093dcb6edc433465394531d897900d19
Khyrki <khyrkimitsunobi@gmail.com>
2021-06-14 18:03:06 -0300

    Add ex9 bloco 25 dia 1

---
+++ bloco_25_MongoDB-Aggregation/ex9.mjs
@@ -0,0 +1,28 @@
+use('erp');
+db.vendas.aggregate(
+  [
+    {
+      $match: {
+        status: { $in: ["EM SEPARACAO", "ENTREGUE"] },
+        dataVenda: { $gte: ISODate("2019-01-01"), $lte: ISODate("2019-12-31") }
+      }
+    },
+    {
+      $group: {
+        _id: "$clienteId",
+
+        valorTotal: {
+          $sum: "$valorTotal"
+        }
+      }
+    },
+    {
+      $sort: {
+        valorTotal: -1
+      }
+    },
+    {
+      $limit: 10
+    }
+  ]
+);
