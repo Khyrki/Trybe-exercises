b69873e0e312325f32421f6485d4135c97c59417
Khyrki <khyrkimitsunobi@gmail.com>
2021-06-14 18:18:34 -0300

    Add ex11 bloco 25 dia 1

---
+++ bloco_25_MongoDB-Aggregation/ex11.mjs
@@ -0,0 +1,23 @@
+use('erp');
+db.vendas.aggregate(
+  [
+    {
+      $match: {
+        dataVenda: { $gte: ISODate("2020-01-01"), $lte: ISODate("2020-03-31") }
+      }
+    },
+    {
+      $group: {
+        _id: "$clienteId",
+        count: {
+          $sum: 1
+        }
+      }
+    },
+    {
+      $match: {
+        count: { $lt: 3 },
+      }
+    }
+  ]
+);
