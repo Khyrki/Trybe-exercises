2263585f9065d27f83607be82702e8e93897439e
Khyrki <khyrkimitsunobi@gmail.com>
2021-08-21 12:21:47 -0300

    Add ex 33.3

---
+++ bloco_33_Python/dia_1/ex3.py
@@ -0,0 +1,12 @@
+""" Faça um programa que, dado um valor n qualquer,
+tal que n > 1 , imprima na tela um quadrado feito
+de asteriscos de lado de tamanho n . Por exemplo: """
+
+
+def draw_square(n):
+    for row in range(n):
+        print(n * '*')
+
+
+draw_square(5)
+
